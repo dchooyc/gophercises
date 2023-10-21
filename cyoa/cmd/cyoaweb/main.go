367c83b09daa6af7a609c3cee91014be5af6f01c
dchooyc <dchooyc@gmail.com>
2023-10-22 07:34:05 +0800

    Serve story on server starting with intro

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"cyoa"
-	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "port to start cyoa")
 	filename := flag.String("file", "gopher.json", "story line json file")
 	flag.Parse()
 	fmt.Printf("Using story in %s.\n", *filename)
 
 	f, err := os.Open(*filename)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error opening file:", err)
 	}
 
-	d := json.NewDecoder(f)
-	var story cyoa.Story
-	if err := d.Decode(&story); err != nil {
-		panic(err)
+	story, err := cyoa.ParseStory(f)
+	if err != nil {
+		fmt.Println("Error parsing story:", err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("starting server on port:%d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- cyoa/story.go
+++ cyoa/story.go
@@ -1,5 +1,39 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Choose your own adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+    {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+    </ul>
+</body>
+</html>`
+
 type Story map[string]Chapter
 
 type Chapter struct {
@@ -11,4 +45,28 @@ type Chapter struct {
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
+}
+
+func ParseStory(file *os.File) (Story, error) {
+	d := json.NewDecoder(file)
+	var story Story
+	if err := d.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+type handler struct {
+	s Story
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
 }
